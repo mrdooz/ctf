6507fcd3b4c357130aa8037a9bf227b52963a3a6
Magnus Österlind <magnus.osterlind@gmail.com>
2015-08-12 08:02:38 +0200

    Bugfixed the solver

--- ring0suduko.py
+++ ring0suduko.py
@@ -19,68 +19,39 @@ def parse_suduko(s):
 
 
 def determine_holes(board):
-    res = []
     for i in range(9):
         for j in range(9):
             if not board[i][j]:
-                res.append((i, j))
-    return res
+                yield (i, j)
 
 
 def candidates(row, col, board):
-    nums = set(range(1, 10))
     rows = set([board[row][i] for i in range(9)])
     cols = set([board[i][col] for i in range(9)])
     rr = row // 3 * 3
     cc = col // 3 * 3
     square = set([board[i+rr][j+cc] for i in range(3) for j in range(3)])
-    return nums.difference(rows | cols | square)
-
-
-def is_solution(board):
-    nums = set(range(1, 10))
-    # check each row
-    for i in range(9):
-        vals = set([board[i][j] for j in range(9)])
-        if nums.difference(vals):
-            return False
-
-    for i in range(9):
-        vals = set([board[j][i] for j in range(9)])
-        if nums.difference(vals):
-            return False
-
-    for i in range(3):
-        for j in range(3):
-            vals = set([board[i*3+ii][j*3+jj] for ii in range(3) for jj in range(3)])
-            if nums.difference(vals):
-                return False
-
-    return True
+    total = rows | cols | square
+    for i in range(1, 10):
+        if i not in total:
+            yield i
 
 
 def solve(board):
-    holes = determine_holes(board)
-    if not holes:
-        return board
-
-    org = deepcopy(board)
-
-    for hole in holes:
+    for hole in determine_holes(board):
         row, col = hole
-        cands = candidates(row, col, board)
-        if not cands:
-            board = org
-            return False
-        for c in cands:
-            tmp = deepcopy(board)
+        for c in candidates(row, col, board):
+            old = board[row][col]
             board[row][col] = c
             b = solve(board)
             if b:
-                return b
-            board = tmp
+                print ','.join([str(x) for x in chain.from_iterable(b)])
+                exit(1)
+            board[row][col] = old
+        else:
+            return False
+    else:
+        return board
 
 board = parse_suduko(open('suduko.txt').read())
 s = solve(board)
-# print s
-print ','.join([str(x) for x in chain.from_iterable(s)])
